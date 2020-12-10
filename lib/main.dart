4c68de20e39b92a5cf9afda7b3c099a8cb764046
Satyam953 <satyamgoyals953@gmail.com>
2020-12-10 10:00:09 +0530

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Linkedin',
+      title: 'Linkedinlogin',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
